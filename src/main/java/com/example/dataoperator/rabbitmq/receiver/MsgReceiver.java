5e650abe051481912d9c7a2d7f8ac6fe84c6af48
Peter_shw <787297031@qq.com>
2019-04-08 21:09:28 +0800

    rabbitmq

--- src/main/java/com/example/dataoperator/rabbitmq/receiver/MsgReceiver.java
+++ src/main/java/com/example/dataoperator/rabbitmq/receiver/MsgReceiver.java
@@ -8,7 +8,7 @@ import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 @Component
-@RabbitListener(queues = RabbitConfig.QUEUE_B)
+@RabbitListener(queues = RabbitConfig.QUEUE_A)
 public class MsgReceiver {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
