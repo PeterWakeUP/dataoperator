b347ee61ab6e4e6256043de0c6ff2317becbf785
Peter_shw <787297031@qq.com>
2020-04-22 14:16:31 +0800

    zk

--- src/main/java/com/example/dataoperator/DataoperatorApplication.java
+++ src/main/java/com/example/dataoperator/DataoperatorApplication.java
@@ -1,9 +1,11 @@
 package com.example.dataoperator;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.example.dataoperator.dao")
 public class DataoperatorApplication {
 
     public static void main(String[] args) {
